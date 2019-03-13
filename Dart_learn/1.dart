672126c8c2d67da8f967428c2074abc1f8caa3d7
zhangweijie <576579227@qq.com>
2019-03-13 18:11:55 +0800

    0313-2

--- Dart_learn/1.dart
+++ Dart_learn/1.dart
@@ -1,10 +1,18 @@
-// 定义个方法。
-printNumber(num aNumber) {
-  print('The number is $aNumber.'); // 在控制台打印内容。
+import 'dart:math';
+
+class Point {
+  final num x;
+  final num y;
+  final num distanceFromOrigin;
+
+  Point(x, y)
+      : x = x,
+        y = y,
+        distanceFromOrigin = sqrt(x * x + y * y);
 }
 
-// 这是程序执行的入口。
 main() {
-  var number = 42; // 定义并初始化一个变量。
-  printNumber(number); // 调用一个方法。
+  var p = new Point(3, 4);
+  print(p.x);
+  print(p.distanceFromOrigin);
 }
